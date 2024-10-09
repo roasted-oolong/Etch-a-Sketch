9cf9ccb74b3f174be82ea1b80a2850a94683d1cd
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2024-10-09 21:56:42 +0000

    Add separate line that adds class to block
    Reason: div.block is not a valid argument

--- js-file.js
+++ js-file.js
@@ -5,7 +5,8 @@ const gridSize = gridNumber * gridNumber;
 function createGrid() {
     let i = 0;
     while (i < gridSize) {
-        const block = document.createElement('div.block');
+        const block = document.createElement('div');
+        block.classList.add('block');
         block.style.width = `${100/gridNumber}%`;
         block.style.height = `${100/gridNumber}%`
         container.appendChild(block);
